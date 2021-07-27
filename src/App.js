b93a508c1017465786ec290042c10c06ea6075e3
Garrett <garrett361@gmail.com>
2021-07-27 06:26:49 -0400

    added weight example

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import unitsSet from './data/unitsSet'
 import presetsSet from './data/presetsSet'
 
 
-
-
 let initialState = {
   input: [],
   outputUnit: 'm',
@@ -56,13 +54,20 @@ class App extends Component {
 
       <div className="container">
         <h1>
-          Natural Units Convertor
+          <a href="https://garrettgoon.com" target="_blank">Natural Units Convertor - garrettgoon.com</a>
         </h1>
 
         <p>
-          It is standard practice in high-energy physics to use <em>natural units</em> in which \(\hbar=c=k_B=e=1\) and to express all dimensionful quantites in terms of a single unit of one's choosing by using \(\hbar, c,k_B,e\) as conversion factors.
-          In the General Relativity community, it's further common to set \(G_N=1\), in which case all quantites can be expressed at unitless numbers.
+          It is standard practice in high-energy physics to use <em>natural units</em> in which
+          \(\hbar=c=k_B=1\) and to express all dimensionful quantites in terms of a single unit
+          of one's choosing by using the preceding quantities as conversion factors.
+          In the General Relativity community, it's further common to set \(G_N=1\), in which case all
+          quantites can be expressed at unitless numbers.
         </p>
+
+        <p><b>Example:</b> I weigh about 190lbs \(\approx\) 86.3kg.  Since \(\hbar/c\) has units of kg * m, we
+          can divide my weight by this product to find that I weigh about 10e44 inverse-meters. Multiplying instead
+           by \(c^{2}/k_B\), I find that I weigh about 10e41 Kelvin.  Finally using units where \(G_N=1\), I find I simply weigh around 10e10.   </p>
 
         <p>
           The calculator below implements such conversions.
